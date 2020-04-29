56f9856a9b418a47dcd6341d4740276734a03130
jisshub <jissmon476@gmail.com>
2020-04-29 13:44:44 +0530

    book and genre feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'digital-library-app';
+
+  // set a current feature
+  currentFeature: string = "books"
+  // get the emitted data from header
+  OnEmitted(selectedF: string){
+    this.currentFeature = selectedF;
+  }
 }
