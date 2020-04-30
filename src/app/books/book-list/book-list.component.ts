54c6efd08e29d40223ebc753eee42d67d3c871cf
jissmon476@gmail.com <jissmon476@gmail.com>
2020-04-30 12:50:17 +0530

    new commit - book details feature added

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -12,12 +12,14 @@ export class BookListComponent implements OnInit {
   @Output() emitToBooks = new EventEmitter<BookModel>()
   // create books array of BooksModel to display in page
   bookArr: BookModel[] = [
-    new BookModel("Harry Potter", 'J K Rowling', 3300, 
+    new BookModel("The Monk Who Sold his ferrari", 'Robin Sharma', 3300, 
     "https://images-na.ssl-images-amazon.com/images/I/81YOuOGFCJL.jpg",
     "Lorem ipsum dolor sit amet consectetur adipisicing elit."),
     new BookModel("Harry Potter", 'J K Rowling', 3300, 
-    "https://images-na.ssl-images-amazon.com/images/I/81YOuOGFCJL.jpg",
+    "https://images.squarespace-cdn.com/content/59aa7d07d2b857b875602871/1505471324923-B5E0YBK5CCWKGMAW0ZYI/Themonk+who+sold+ferrari.jpg",
     "Lorem ipsum dolor sit amet consectetur adipisicing elit."),
+    new BookModel("The Great Gatsby","Scott Fitzgerald", 4500, "https://kbimages1-a.akamaihd.net/cb34b7ac-2d03-481f-b361-6e92113234c8/1200/1200/False/the-great-gatsby-13.jpg",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit." )
   ];
 
   constructor() {}
